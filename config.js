d5babb288d8488641f65f242999992ae51ce7d38
EVGEN <inevitability.k@gmail.com>
2022-05-23 11:22:59 +0300

    Добавил конфиг для сборки

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  styles: {
+    'App/client/styles/client.min.css': './src/client/styles/index.less',
+  },
+  scripts: {
+    'App/client/scripts/client.min.js': './src/client/scripts/index.js',
+  },
+};
