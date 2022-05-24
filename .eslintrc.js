ac2336a3f819bd232e5ed84f7f54ddbd6c3a7e1d
EVGEN <inevitability.k@gmail.com>
2022-05-24 18:51:50 +0300

    eslint rulse

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,29 @@
-module.exports = { // eslint-disable-line
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:vue/essential'
-    ],
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'vue'
-    ],
-    'rules': {
-        'semi': ['error', 'always'],
-        'quotes': ['error', 'single']
-    }
+/* eslint-disable-next-line */
+module.exports = {
+  'extends': [
+    'eslint:recommended',
+    'plugin:vue/essential',
+  ],
+  'plugins': [
+    'vue',
+  ],
+  'rules': {
+    'semi': [2, 'always'],
+    'quotes': [2, 'single'],
+    'indent': [2, 2],
+    'comma-dangle': [2, 'always-multiline'],
+    'comma-spacing': [2, {
+      before: false,
+    }],
+    'no-trailing-spaces': [2],
+    'eol-last': [2, 'always'],
+    'no-multiple-empty-lines': [2, {
+      max: 1,
+    }],
+    'no-use-before-define': [2, {
+      functions: false,
+      classes: true,
+      variables: true,
+    }],
+  },
 };
