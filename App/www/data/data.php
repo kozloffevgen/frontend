9f4037eb23fe679ee1a34cfb47e826870dec36fd
EVGEN <inevitability.k@gmail.com>
2022-05-31 08:40:45 +0300

    Data main block

--- App/www/data/data.php
+++ App/www/data/data.php
@@ -23,4 +23,41 @@ return [
       'Partner' => [],
     ],
   ],
+  'dataMain' => [
+    'btnText' => 'Read More',
+    'firstBlock' => [
+      'title' => 'Making the most of the ever-growing<p class="chosen-text">Information Technology</p>',
+      'svgPana' => './www/images/sprites/sprite.svg#pana',
+      'text' => 'Managed by a team of professional experts with extensive experience and impressive track records',
+    ],
+    'secondBlock' => [
+      'title' => 'Welcome to <span class="chosen-text">Dwidasa Samsara Indonesia (DSI)</span>',
+      'svgWelcome' => './www/images/sprites/sprite.svg#welcome',
+      'text' => 'Dwidasa Samsara Indonesia (DSI) was firstly established in 2010 by the founders, who each of them has a common end objective to innovate new creations by making the most of the ever-growing information technology through DSI’s distinct front-end based application concept.
+                <br>
+                <br>
+                Managed by a team of professional experts with extensive experience and impressive track records, DSI believes that continuous improvements and innovations assure your business to run effectively and efficiently.',
+    ],
+    'thirdBlock' => [
+      'title' => 'Product and Service',
+      'items'=> [
+        'product' => [
+          'svg' => './www/images/sprites/sprite.svg#product',
+          'title' => 'Our Product',
+          'text' => 'Our product is made on the base of our team’s careful research and analyses, ranging from internet based application.',
+        ],
+        'service' => [
+          'svg' => './www/images/sprites/sprite.svg#service',
+          'title' => 'Our Service',
+          'text' => 'DSI’s shared service solutions focus on the front-end based software development, designed specifically for the banking and financial sectors.
+          ',
+        ],
+        'technology' => [
+          'svg' => './www/images/sprites/sprite.svg#technology',
+          'title' => 'Our Technology',
+          'text' => 'First JAVA, runs on more than 850 million personal computers worldwide, and on billions of devices worldwide, including mobile and TV devices.',
+        ],
+      ],
+    ],
+  ],
 ];
